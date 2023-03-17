981bbfb79e53c9e612f20d5abb5546a8ed4fa67d
Lenovo <zaferdemirelgsb@gmail.com>
2023-03-17 23:13:05 +0300

    yeniKelimeUret method is added

--- src/test/java/CountOfLetter.java
+++ src/test/java/CountOfLetter.java
@@ -8,9 +8,11 @@ public class CountOfLetter {
 //        System.out.println(countLetter("karaaarrryyyabacak"));
 //        System.out.println("countOfLetter(\"karabacak\") = " + countOfLetter("karabacak"));
 
-        int [] xarr={10,20,30,60,50,5};
+//        int[] xarr = {10, 20, 30, 60, 50, 5};
 //        System.out.println("bigOne(xarr) = " + maxOne(xarr));
-        System.out.println("minOne(xarr) = " + minOne(xarr));
+//        System.out.println("minOne(xarr) = " + minOne(xarr));
+
+        System.out.println(yeniKelimeUret("ün görmüştü"));
 
 
     }
@@ -32,25 +34,25 @@ public class CountOfLetter {
 
             list.add(count + "" + letter);
             //count = 0;
-            str=str.replaceAll(str.substring(0, 1), "");
+            str = str.replaceAll(str.substring(0, 1), "");
 
 
         }
         return list;
     }
 
-    public static List<String> countOfLetter(String str){
+    public static List<String> countOfLetter(String str) {
 
-        List<String> newList=new ArrayList<>();
+        List<String> newList = new ArrayList<>();
 
-        while(!str.isEmpty()){
+        while (!str.isEmpty()) {
 
             String firstLetter = str.substring(0, 1);
             int countLetter = str.length() - (str.replaceAll(firstLetter, "").length());
 
-            newList.add(countLetter + ""+ firstLetter);
+            newList.add(countLetter + "" + firstLetter);
 
-            str=str.replaceAll(firstLetter,"");
+            str = str.replaceAll(firstLetter, "");
         }
         return newList;
 
@@ -59,31 +61,64 @@ public class CountOfLetter {
     //    İnt arry in en büyük elemanının döndüren bir method yazın
 
 
-    public static int maxOne(int[] arr){
+    public static int maxOne(int[] arr) {
 
-        int max=arr[0];
+        int max = arr[0];
 
-        for (Integer box: arr) {
-            if(box>max){
-                max=box;
+        for (Integer box : arr) {
+            if (box > max) {
+                max = box;
             }
         }
 
         return max;
     }
 
-    public static int minOne(int [] arr){
+    public static int minOne(int[] arr) {
 
-        int min=arr[0];
+        int min = arr[0];
 
-        for (Integer box:arr) {
-            if(box<min){
-                min=box;
+        for (Integer box : arr) {
+            if (box < min) {
+                min = box;
             }
         }
         return min;
 
     }
+
+
+    /*
+    //Dün akşam eve giderken onları görmüştüm.
+// Cümlesindeki ü harfinin önünde
+// ve arkasında olan harflerden bir string üreten bir method yazın.
+     */
+
+    public static String yeniKelimeUret(String str) {
+
+        String result="";
+        //String str="ünlü akşam eve giderken onları görmüştü";
+        if(str.substring(0,1).equals("ü")) result+=str.substring(1,2);
+
+
+        for(int i=1; i<str.length()-1; i++){
+
+            if(str.substring(i,i+1).equals("ü")){
+                result+=str.substring(i-1,i);
+                result+=str.substring(i+1,i+2);
+            }
+        }
+
+
+        if(str.substring(str.length()-1).equals("ü")) result+=str.substring(str.length()-2,str.length()-1);
+
+        return result;
+
+    }
+
+
+
+
 
 
     /*
@@ -94,7 +129,6 @@ public class CountOfLetter {
     or
     - String str2 = String.valueOf(num);
      */
-
 
 
 }
