6d1b57c4f156472631d30ba0fdb2516492776ce7
Lenovo <zaferdemirelgsb@gmail.com>
2023-03-28 18:26:50 +0300

    removePolindromWords method is added

---
+++ src/test/java/PolindromWords.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class PolindromWords {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("reverseWord(\"zafer\") = " + reverseWord("zafer"));
+
+        String [] liste={"ey",  "edip", "adanada", "pide", "ye"};
+
+        removePolindrom(Arrays.asList(liste));
+
+    }
+
+
+
+    //    // interviev questions List<String>
+    //    // List içerisindeki palindrom kelimeleri list içerinden silen metodu yazınız.
+    //    //ey,  edip, adanada, pide, ye  ey,  edip,pide, ye
+
+
+
+    public static String reverseWord(String str){
+
+        String reverse="";
+
+        for (int i = str.length()-1; 0 <= i; i--) {
+
+            reverse+=str.charAt(i);
+        }
+        return reverse;
+    }
+
+    public static List<String> removePolindrom(List<String> list){
+
+
+        for (int i = 0; i < list.size(); ) {
+
+            if (list.get(i).equalsIgnoreCase(reverseWord(list.get(i)))){
+                list.remove(list.get(i));
+                continue;
+
+            }
+            i++;
+
+
+        }
+
+        return list;
+
+    }
+
+
+
+
+
+}
