82f4941d854eff5cb1fd86a90b305ab0180b900f
Lenovo <zaferdemirelgsb@gmail.com>
2023-04-06 00:12:42 +0300

    Hackerrank Questions 2 and 3 are added

---
+++ src/test/java/HackerRank_Question_2.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class HackerRank_Question_2 {
+
+
+    /*
+    Task
+Given an integer, n, perform the following conditional actions:
+
+If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20, print Weird
+If n is even and greater than 20, print Not Weird
+Complete the stub code provided in your editor to print whether or not n is weird.
+     */
+
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        if(n%2==0){
+            if(2<=n && n<5){
+                System.out.println("Not Weird");
+            }else if(6<=n && n<=20){
+                System.out.println("Weird");
+            }else if(n>20){
+                System.out.println("Not Weird");
+            }else {
+                System.out.println("weird");
+            }
+
+        } else if(n%2==1){
+            System.out.println("Weird");
+        }
+
+
+        scanner.close();
+
+
+    }
+}
