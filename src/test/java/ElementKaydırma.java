76366410b9083ed2662a97f8fbedd9e3a9bd781e
Lenovo <zaferdemirelgsb@gmail.com>
2023-04-11 15:02:57 +0300

    Question was written

---
+++ src/test/java/ElementKaydırma.java
@@ -0,0 +1,12 @@
+public class ElementKaydırma {
+
+    /*
+    Verilen bir arraydeki tüm elementleri bir sağa kaydırıp
+    sondaki elementi de en başa taşıyacak bir method oluşturun
+    Orn: Input [4,5,6,7]    output [7,4,5,6]
+     */
+
+
+
+
+}
