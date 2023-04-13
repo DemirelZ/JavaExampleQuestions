6c91a31095db5340ba79b9c62b4bb39aa4ec8424
Lenovo <zaferdemirelgsb@gmail.com>
2023-04-13 17:09:10 +0300

    Soru çözülüyor

--- src/test/java/ElementKaydırma.java
+++ src/test/java/ElementKaydırma.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class ElementKaydırma {
 
     /*
@@ -6,7 +8,32 @@ public class ElementKaydırma {
     Orn: Input [4,5,6,7]    output [7,4,5,6]
      */
 
+    public static void main(String[] args) {
 
+        int [] arrOrn= {4,5,6,7};
+
+        System.out.println("arrOrn = " + Arrays.toString(arrOrn));
+
+
+
+        //System.out.println(sondakiBasa(Arrays.toString(arrOrn)));
+
+    }
+
+    public static int [] sondakiBasa(int [] arr){
+
+        int[] arrNew=new int[arr.length];
+        arrNew[0]=arr[arr.length-1];
+        for (int i = 0, j=1; i < arr.length-1; i++, j++) {
+
+            arrNew[j]=arr[i];
+
+
+        }
+
+        return  arrNew;
+
+    }
 
 
 }
