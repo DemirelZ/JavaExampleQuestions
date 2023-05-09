221b9e356579e426a81418c5c3d0cb5f0efec423
Lenovo <zaferdemirelgsb@gmail.com>
2023-05-09 23:29:52 +0300

    enCokSayiyiBulanMetot method was added

---
+++ src/test/java/EnCokSayi.java
@@ -0,0 +1,22 @@
+public class EnCokSayi {
+
+
+
+
+
+
+    //Array’in içerisinde en çok bulunan sayıyı veren metodu yazınız.
+    // (eşitlik durumda herhangi birini alabilir,1 den  bulunmuyorsa -1 döndürecek)
+    //[2,3,3,3,6,6,5,6,3]  3
+    //
+    //[2,4,5,8,8]  8
+    //
+    //[2,4,5,8]  -1
+
+    public static int enCokSayiyiBulanMetot(){
+
+
+
+    }
+
+}
