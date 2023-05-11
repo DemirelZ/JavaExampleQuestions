7a34c809dd506ea9b1e0223728435e3ad0374ff1
Lenovo <zaferdemirelgsb@gmail.com>
2023-05-11 21:58:41 +0300

    isRotaion method was added

---
+++ src/test/java/RotationQuestion.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class RotationQuestion {
+
+    public static void main(String[] args) {
+
+        System.out.println(isRotation("car", "car"));
+
+    }
+
+
+    //which method would you use to learn if a string is a rotation of another string?
+    //rotation of car --> car, arc, rca
+    // sari --> sari, aris, risa, isar
+
+    public static boolean isRotation(String word, String rotation){
+
+
+        List<String> list=new ArrayList<>();
+
+        System.out.println(list);
+
+
+        for (int i = 0; i < word.length(); i++) {
+
+            word = word.substring(1) + word.substring(0, 1);
+
+            list.add(word);
+
+        }
+
+        System.out.println(list);
+
+        return list.contains(rotation);
+
+    }
+
+
+
+}
