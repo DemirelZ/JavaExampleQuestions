6185da8a868822ec1159256cd0145b46477ec957
Lenovo <zaferdemirelgsb@gmail.com>
2023-05-18 22:33:17 +0300

    eksikTekSayiBul question is added

---
+++ src/test/java/EksikTekSayi.java
@@ -0,0 +1,24 @@
+public class EksikTekSayi {
+
+
+    /**
+     * Assume that we have an array of positive integers from m to n which includes all odd
+     * numbers from m to n except one.
+     * Write a function that finds the missing odd number in the list.
+     *
+     * Ex.:
+     * Input :[1,3,5,7,11,13,15]
+     * Output: 11
+     *
+     * Input:[9,7,3]
+     * Output:5
+     *
+     * Constraints:
+     * 1<length of the input list(nums)<100
+     * There is always a single missing number
+     * Array is sorted either descending or ascending.
+     */
+
+
+
+}
