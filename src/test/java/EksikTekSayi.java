462112978a203c174f064dc93b107b99d4033562
Lenovo <zaferdemirelgsb@gmail.com>
2023-05-18 22:54:25 +0300

    eksikTekSayiBul method is modified

--- src/test/java/EksikTekSayi.java
+++ src/test/java/EksikTekSayi.java
@@ -5,14 +5,14 @@ public class EksikTekSayi {
      * Assume that we have an array of positive integers from m to n which includes all odd
      * numbers from m to n except one.
      * Write a function that finds the missing odd number in the list.
-     *
+     * <p>
      * Ex.:
      * Input :[1,3,5,7,11,13,15]
      * Output: 11
-     *
+     * <p>
      * Input:[9,7,3]
      * Output:5
-     *
+     * <p>
      * Constraints:
      * 1<length of the input list(nums)<100
      * There is always a single missing number
@@ -22,8 +22,8 @@ public class EksikTekSayi {
 
     public static void main(String[] args) {
 
-        Integer[] exArr={1,3,5,7,11,13,15};
-        Integer[] exArr2={9,7,3};
+        Integer[] exArr = {1, 3, 5, 7, 11, 13, 15};
+        Integer[] exArr2 = {9, 7, 3};
 
         System.out.println(eksikTekSayiBul(exArr2));
 
@@ -32,21 +32,38 @@ public class EksikTekSayi {
 
     public static int eksikTekSayiBul(Integer[] arr) {
 
-        for (int i = 0; i < arr.length; i++) {
 
-            if(arr[i]-arr[i+1]==-4){
+        if (arr.length < 100) {
 
-                return arr[i]+2;
-            }else if(arr[i]-arr[i+1]==4){
+            for (int i = 0; i < arr.length; i++) {
 
-                return arr[i]-2;
+                if (arr[i] - arr[i + 1] == -4) {
+
+                    return arr[i] + 2;
+                } else if (arr[i] - arr[i + 1] == 4) {
+
+                    return arr[i] - 2;
+                }
+
             }
 
-
-
+        } else {
+            System.out.println("Please, length of the input array should be less than 100");
         }
 
         return -1;
     }
+
+    public static int findMissingOddNumber(int[] nums) {
+        int length = nums.length;
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+        int expectedSum = (nums[0] + nums[length - 1]) * length / 2;
+        int missingNumber = expectedSum - totalSum;
+        return missingNumber;
+    }
+
 
 }
