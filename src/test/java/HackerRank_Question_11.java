247d8d7cb728de3f02eb31619bbf27005b21d249
Lenovo <zaferdemirelgsb@gmail.com>
2023-05-31 23:28:35 +0300

    HackerRank_Question_11 is added

---
+++ src/test/java/HackerRank_Question_11.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class HackerRank_Question_11 {
+
+    /*
+    A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward.
+    Given a string A, print Yes if it is a palindrome, print No otherwise.
+
+    Sample Input
+    madam
+
+    Sample Output
+    yes
+     */
+
+    public static void main(String[] args) {
+
+        Scanner scan=new Scanner(System.in);
+        String A = scan.next();
+
+
+
+
+    }
+}
