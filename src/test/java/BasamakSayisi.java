60481b50f218025e57ed55199b5ca10ab9d68c5f
Lenovo <zaferdemirelgsb@gmail.com>
2023-06-06 22:21:13 +0300

    BasamakSayisi method is added

--- src/test/java/BasamakSayisi.java
+++ src/test/java/BasamakSayisi.java
@@ -2,9 +2,20 @@ public class BasamakSayisi {
 
     //Kullanıcıdan alınan sayının kaç basamaklı olduğunu bulan metodu yazınız
 
+    public static void main(String[] args) {
 
-    public static void basamakSayisi(int sayi){
+
+        basamakSayisi("1");
+    }
+
+    public static void basamakSayisi(String sayi){
 
 
+        int count=0;
+        for (int i = 0; i < sayi.length(); i++) {
+            count++;
+        }
+
+        System.out.println("Sayı " + count + " basamaklıdır");
     }
 }
